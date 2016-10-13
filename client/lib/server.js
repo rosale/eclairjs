579c1054e797a68e7dcb449bbf4dce0d65e7de06
Doron Rosenberg <doronr@us.ibm.com>
2016-10-13 14:48:40 -0700

    Branch for jupyter add jars magic support.  use env variable ECLAIRJS_ADD_MAGIC_JAR for the jar location and ECLAIRJS_KERNEL_NAME for the custom kernel name

--- client/lib/server.js
+++ client/lib/server.js
@@ -30,7 +30,18 @@ function Server() {
   this.kernelP = new Promise(function(resolve, reject) {
     scope.kernelPResolve = function(kernelSession) {
       scope.session = kernelSession;
-      resolve(kernelSession.kernel)
+
+      if (process.env.ECLAIRJS_ADD_MAGIC_JAR) {
+        var c = kernelSession.kernel.execute({code: '%AddJar --magic '+process.env.ECLAIRJS_ADD_MAGIC_JAR});
+        kernel.verifyKernelExecution(c, function() {
+          resolve(kernelSession.kernel);
+        }, function(e) {
+          console.error(e);
+          reject(e);
+        });
+      } else {
+        resolve(kernelSession.kernel);
+      }
     };
 
     scope.kernelPReject = function(e) {
